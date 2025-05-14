568b5bc43f14e76fcd50c5ec89fcce139842765d
devakapatel <dev.akash.patel@gmail.com>
2025-05-14 19:23:17 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 import nextra from "nextra";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 // Set up Nextra with its configuration
 const withNextra = nextra({
   // ... Add Nextra-specific options here
 });
 
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
 // Export the final Next.js config with Nextra included
 export default withNextra({
-  distDir: 'out',
   // ... Add regular Next.js options here
 });
